7deca8156852e97fc39137044f07e64176445bb3
Kevin Kamto <kamtokevin@gmail.com>
2019-10-13 01:10:22 -0400

    feat: added MainView

---
+++ Views/MainView.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Container, Header, Footer, Content} from 'native-base';
+
+
+class MainView extends Component {
+
+    render() {
+
+        return
+        (
+            <Container>
+                <Header title="Main View"></Header>
+                <Content>
+                    {/* Components go in here */}
+                    
+                </Content>
+            </Container>
+        );
+    }
+}
