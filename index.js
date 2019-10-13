fdc9d8cf730e2a845f02a5225a9f2de204caa605
Kevin Kamto <kamtokevin@gmail.com>
2019-10-13 01:10:05 -0400

    chore: added firebase module + config

--- index.js
+++ index.js
@@ -2,4 +2,19 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
+// Firebase Setup
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "<your-api-key>",
+  authDomain: "<your-auth-domain>",
+  databaseURL: "<your-database-url>",
+  storageBucket: "<your-storage-bucket>",
+}
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseDB = firebaseApp.database().ref();
+
 AppRegistry.registerComponent(appName, () => App);
+
+export { firebaseApp as fireApp, firebaseDB as fireDB };
