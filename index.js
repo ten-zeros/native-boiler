3208b89a4d0729e5f4df263eb74880a49858f34a
Kevin Kamto <kamtokevin@gmail.com>
2019-10-17 02:41:12 -0400

    feat: added basic auth view

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+// import App from './App';
+import App from './Views/AuthView';
 import { name as appName } from './app.json';
 
-// Firebase Setup
-import * as firebase from 'firebase';
-
-const firebaseConfig = {
-  apiKey: "<your-api-key>",
-  authDomain: "<your-auth-domain>",
-  databaseURL: "<your-database-url>",
-  storageBucket: "<your-storage-bucket>",
-}
-
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const firebaseDB = firebaseApp.database().ref();
-
 AppRegistry.registerComponent(appName, () => App);
-
-export { firebaseApp as fireApp, firebaseDB as fireDB };
