b5c10b6dcbf34e6804cc77b4109701210f751852
Kevin Kamto <kamtokevin@gmail.com>
2020-01-05 03:11:06 -0500

    refactor: laid groundwork for stack navigation

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import MainView from './Views/MainView';
-import LoginView from './Views/LoginView';
+// import LoginView from './Views/LoginView';
+// import {createAppContainer} from 'react-navigation';
+// import {createStackNavigator} from 'react-navigation-stack';
 
+// // Different Screens + Their names
+// const Navigator = createStackNavigator({
+//   Home: {screen: MainView},
+//   Profile: {screen: LoginView}
+// }, 
+// {
+//   initialRouteName: 'Home'
+// })
 
+// // App container
+// const Application = createAppContainer(Navigator);
   
 export default class App extends Component {
 
